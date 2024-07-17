610c6fc0a19f44a27bd096650ff53ac50677b0b6
Shriram Mange <110598631+Shriram2005@users.noreply.github.com>
2024-07-17 15:57:21 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,155 @@
+// for video 1
+var hoverDiv = document.getElementById('video1');
+var targetDiv = document.getElementById('center-text');
+
+hoverDiv.addEventListener('mouseover', function() {
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'This is video 1';
+    targetDiv.classList.remove('hide');
+    targetDiv.classList.add('show');
+    targetDiv.style.zIndex=1;
+  }, 300);
+});
+
+hoverDiv.addEventListener('mouseout', function() {
+  targetDiv.classList.remove('show');
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'Interactive Studio';
+    targetDiv.classList.remove('hide');
+    targetDiv.style.zIndex=-1;
+  }, 300);
+});
+
+
+// for video 2
+var hoverDiv = document.getElementById('video2');
+var targetDiv = document.getElementById('center-text');
+
+hoverDiv.addEventListener('mouseover', function() {
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'This is video 2';
+    targetDiv.classList.remove('hide');
+    targetDiv.classList.add('show');
+    targetDiv.style.zIndex=1;
+  }, 300);
+});
+
+hoverDiv.addEventListener('mouseout', function() {
+  targetDiv.classList.remove('show');
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'Interactive Studio';
+    targetDiv.classList.remove('hide');
+    targetDiv.style.zIndex=-1;
+  }, 300);
+});
+
+
+// for video 3
+var hoverDiv = document.getElementById('video3');
+var targetDiv = document.getElementById('center-text');
+
+hoverDiv.addEventListener('mouseover', function() {
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'This is video 3';
+    targetDiv.classList.remove('hide');
+    targetDiv.classList.add('show');
+    targetDiv.style.zIndex=1;
+  }, 300);
+});
+
+hoverDiv.addEventListener('mouseout', function() {
+  targetDiv.classList.remove('show');
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'Interactive Studio';
+    targetDiv.classList.remove('hide');
+    targetDiv.style.zIndex=-1;
+  }, 300);
+});
+
+
+// for video 4
+var hoverDiv = document.getElementById('video4');
+var targetDiv = document.getElementById('center-text');
+
+hoverDiv.addEventListener('mouseover', function() {
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'This is video 4';
+    targetDiv.classList.remove('hide');
+    targetDiv.classList.add('show');
+    targetDiv.style.zIndex=1;
+  }, 300);
+});
+
+hoverDiv.addEventListener('mouseout', function() {
+  targetDiv.classList.remove('show');
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'Interactive Studio';
+    targetDiv.classList.remove('hide');
+    targetDiv.style.zIndex=-1;
+  }, 300);
+});
+
+
+// for video 5
+var hoverDiv = document.getElementById('video5');
+var targetDiv = document.getElementById('center-text');
+
+hoverDiv.addEventListener('mouseover', function() {
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'This is video 5';
+    targetDiv.classList.remove('hide');
+    targetDiv.classList.add('show');
+    targetDiv.style.zIndex=1;
+  }, 300);
+});
+
+hoverDiv.addEventListener('mouseout', function() {
+  targetDiv.classList.remove('show');
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'Interactive Studio';
+    targetDiv.classList.remove('hide');
+    targetDiv.style.zIndex=-1;
+  }, 300);
+});
+
+
+// for video 6
+var hoverDiv = document.getElementById('video6');
+var targetDiv = document.getElementById('center-text');
+
+hoverDiv.addEventListener('mouseover', function() {
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'This is video 6';
+    targetDiv.classList.remove('hide');
+    targetDiv.classList.add('show');
+    targetDiv.style.zIndex=1;
+  }, 300);
+});
+
+hoverDiv.addEventListener('mouseout', function() {
+  targetDiv.classList.remove('show');
+  targetDiv.classList.add('hide');
+  setTimeout(function() {
+    targetDiv.textContent = 'Interactive Studio';
+    targetDiv.classList.remove('hide');
+    targetDiv.style.zIndex=-1;
+  }, 300);
+});
+
+
+
+
+
+
+
